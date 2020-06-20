432104954093f5fed8c959b81dc149b8aa6c71b9
Erick <Erick@LAPTOP-C3O5DOD7>
2020-06-20 11:45:15 -0500

    Clase nodo para lista seceuncial. Por: Erick Carrasco

---
+++ ProyectoCompraArticulos/src/ListaSecuencial/Nodo.java
@@ -0,0 +1,23 @@
+package ListaSecuencial;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Erick
+ */
+public class Nodo {
+    public Object dato;
+    public Nodo siguiente;
+
+    public Nodo(Object dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+    
+       
+}
