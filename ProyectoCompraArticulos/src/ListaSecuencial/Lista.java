d6cfbe23953d1a6f0769bc9d22e266534800b501
patyj <patyj@DESKTOP-V3BSKVK>
2020-06-24 22:31:50 -0500

    se realizo metodo Eliminar_Borrar

--- ProyectoCompraArticulos/src/ListaSecuencial/Lista.java
+++ ProyectoCompraArticulos/src/ListaSecuencial/Lista.java
@@ -182,27 +182,7 @@ public class Lista {
             return false;
         }
     }
-     boolean Eliminar_Borrar(int pos ) {
-        if (this.primero == null || pos < 0) {
-            return false;
-        }
-        if (pos == 0) {
-            this.primero = this.primero.siguiente;
-            return true;
-        }
-        int posActual = 1;
-        Nodo aux = this.primero;
-        while (posActual < pos && aux != null) {
-            aux = aux.siguiente;
-            posActual++;
-        }
-        if (aux != null && aux.siguiente != null) {
-            aux.siguiente = aux.siguiente.siguiente;
-            return true;
-        } else {
-            return false;
-        }
-    }
+    
     void imprimir() {
         if (this.primero != null) {
             Nodo aux = this.primero;
